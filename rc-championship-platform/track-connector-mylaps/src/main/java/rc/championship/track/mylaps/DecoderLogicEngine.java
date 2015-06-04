fa46115725519a3b9051fddb7e1b9437b692cc14
skoky <skokys@gmail.com>
2015-06-04 23:25:34 +0200

    added P3 parser

--- rc-championship-platform/track-connector-mylaps/src/main/java/rc/championship/track/mylaps/DecoderLogicEngine.java
+++ rc-championship-platform/track-connector-mylaps/src/main/java/rc/championship/track/mylaps/DecoderLogicEngine.java
@@ -2,6 +2,11 @@ package rc.championship.track.mylaps;
 
 import java.nio.ByteBuffer;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+import eu.plib.P3tools.MsgProcessor;
+import eu.plib.P3tools.data.msg.v2.Passing;
+import eu.plib.Ptools.Message;
 import rc.championship.api.model.Lap;
 import rc.championship.api.services.ConnectorListener;
 import rc.championship.api.services.TrackConnector;
@@ -11,6 +16,8 @@ import rc.championship.api.services.TrackConnector;
  * @author Stefan
  */
 public class DecoderLogicEngine implements Runnable, ConnectorListener {
+
+    private static final Logger LOG = Logger.getLogger(DecoderLogicEngine.class.getName());
 
     private static final int TIMEOUT = 200;
     
@@ -57,8 +64,15 @@ public class DecoderLogicEngine implements Runnable, ConnectorListener {
     }
 
     private void processInconing(ByteBuffer msg) {
-        ///TODO parse p3 msg
-        
+        Message parsedMsg = new MsgProcessor(true).parse(msg.array());
+
+        String parsedMsgJson = parsedMsg.toString();
+        LOG.fine("Received msg "+parsedMsgJson);
+
+        if (parsedMsg.getType().getName().equals(Passing.class.getName())) {
+            Passing passing = (Passing) parsedMsg;
+            LOG.fine("Passing message transponder: "+passing.transponder);
+        }
 //        ByteBuffer response = null;
 //        boolean accepted = connector.sendMessage(response, TIMEOUT, TimeUnit.MILLISECONDS);
     }
